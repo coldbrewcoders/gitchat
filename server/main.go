e64787b9aa7375fff085564fbf888db60baa9dd3
Malcolm Navarro <mnavarro@templuminc.com>
2020-10-06 22:26:27 -0500

    Added basic go server

---
+++ server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Starting server!")
+
+	setupRoutes()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
